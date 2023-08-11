5b8c9038b053fb62f6fdceabe3ae0020220b5f4f
vimal8533 <95617973+vimal8533@users.noreply.github.com>
2023-08-11 14:10:30 +0530

    Update RouteComponent.js

--- src/Component/RouteComponent.js
+++ src/Component/RouteComponent.js
@@ -1,27 +1,23 @@
 import React from "react";
-import { Routes ,Route, BrowserRouter} from "react-router-dom";
+import { Routes ,Route} from "react-router-dom";
 
 import PureComponent from "./PureComponent";
 import Home from "./Home";
 import HocDisplay from "./HocDisplay";
-import LinkComponent from "./LinkComponent";
+
 
 
 const RouteComponent =()=>{
 
     return(
         <>
-        <BrowserRouter>
-        <LinkComponent/>
+        
         <Routes>
                 <Route path="/" element={<Home/>}></Route>
                 <Route path="/hoc" element={<HocDisplay/>}></Route>
                 <Route path="/pure_component" element={<PureComponent/>}></Route>
 
-            </Routes>
-        
-        </BrowserRouter>
-            
+          
         
         </>
     )
